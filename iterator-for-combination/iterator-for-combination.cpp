eae349885a67c40caf67614e8ff8bc40f457190b
AJAY KASWA <68346520+ajaykaswa@users.noreply.github.com>
2021-07-11 14:39:25 +0530

    Time: 8 ms (92.99%), Space: 12.9 MB (25.65%) - LeetHub

---
+++ iterator-for-combination/iterator-for-combination.cpp
@@ -0,0 +1,36 @@
+class CombinationIterator {
+public:
+       vector<string> temp;
+        int i;
+    CombinationIterator(string characters, int combinationlength) {
+            i=0;
+        
+        backtrack(characters,combinationlength,0,"");
+    }
+    void backtrack(string characters,int combinationlength,int idx,string ans){
+        if(ans.size()==combinationlength){
+            temp.push_back(ans);
+            return;
+        }
+        for(int i=idx;i<characters.size();i++){
+            ans.push_back(characters[i]);
+            backtrack(characters,combinationlength,i+1,ans);
+            ans.pop_back();
+        }
+    }
+    
+    string next() {
+        return temp[i++];
+    }
+    
+    bool hasNext() {
+        return i<temp.size();
+    }
+};
+
+/**
+ * Your CombinationIterator object will be instantiated and called as such:
+ * CombinationIterator* obj = new CombinationIterator(characters, combinationLength);
+ * string param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
