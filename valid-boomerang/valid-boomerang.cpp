39fc23bcdc8fb47947a8fc3ba6c2ef8fce7a1536
AJAY KASWA <68346520+ajaykaswa@users.noreply.github.com>
2021-07-24 12:22:24 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (51.09%) - LeetHub

---
+++ valid-boomerang/valid-boomerang.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>>& co) {
+          int y=(co[1][1]-co[0][1]);
+         int x=(co[1][0]-co[0][0]);
+        for(int i=2;i<co.size();i++){
+            int a=(co[i][1]-co[0][1]);
+            int b=(co[i][0]-co[0][0]);
+            if((x*a)!=(y*b)) return true;
+        }
+        return false;
+    }
+};
