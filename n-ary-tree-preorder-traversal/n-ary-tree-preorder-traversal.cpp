065e83f5a47995b8bfb7d71dca0e8c96ee516851
AJAY KASWA <68346520+ajaykaswa@users.noreply.github.com>
2021-09-02 21:24:05 +0530

    Time: 51 ms (5.00%), Space: 11 MB (90.66%) - LeetHub

--- n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.cpp
+++ n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.cpp
@@ -20,17 +20,42 @@ public:
 
 class Solution {
 public:
-   void find(Node* root, vector<int>& ans){
-     if(root==NULL) return ;
-       ans.push_back(root->val);
-     for(auto child:root->children){
-       find(child,ans);
+//    void find(Node* root, vector<int>& ans){
+//      if(root==NULL) return ;
+//        ans.push_back(root->val);
+//      for(auto child:root->children){
+//        find(child,ans);
+//      }
+//    }
+  
+//     vector<int> preorder(Node* root) {
+//         vector<int> ans;
+//       find(root,ans);
+//       return ans;
+//     }
+// };
+
+
+
+ vector<int> preorder(Node* root) {
+   if(root==NULL) return {};
+   stack<Node*> st;
+   st.push(root);
+   vector<int>ans;
+   while(!st.empty()){
+     Node* node=st.top();
+     st.pop();
+     ans.push_back(node->val);
+     int n=node->children.size();
+     for(int i=n-1;i>=0;i--){
+       st.push(node->children[i]);
      }
    }
-  
-    vector<int> preorder(Node* root) {
-        vector<int> ans;
-      find(root,ans);
-      return ans;
-    }
+   return ans;
+ }
 };
+
+
+
+
+
